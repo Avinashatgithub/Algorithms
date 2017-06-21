ce2b67e7fc7be8741b30b7e90e6b89f801048267
Avinash <avinash.k2u@gmail.com>
2017-06-21 13:23:56 +0530

    Update InnerClassTest.java

--- InnerClassTest.java
+++ InnerClassTest.java
@@ -1,1 +1,25 @@
+interface Interface {
+  void run();
+  class InnerClass{
+    static{
+    	System.out.println("InnerClass static block");
+    }
+    {
+    	System.out.println("InnerClass instance block");
+    }
+  }
+  
+}
 
+public class Test{
+  public static void main(String[] args) {
+    Interface inf = new Interface(){
+      public void run(){
+      	System.out.println("Running............");
+        InnerClass c = new InnerClass();
+      }
+    };
+    
+    inf.run();
+  }
+}
